b5301fa580cc9c03b0805aaf5d1392545e386940
rmr327 <36940292+rmr327@users.noreply.github.com>
2023-09-06 14:46:10 -0400

    Update test_PandasDescriptiveStatistics.py

--- tests/test_PandasDescriptiveStatistics.py
+++ tests/test_PandasDescriptiveStatistics.py
@@ -4,11 +4,11 @@ import pandas as pd
 
 def test_return_more_than_25th_quantile():
     data = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
-    tagert_column = 'sepal_width'
+    target_column = 'sepal_width'
 
     res =  return_more_than_25th_quantile(data, target_column)
     
     target_quantile = df[target_column].quantile(0.25)
-    data =  data.loc[data[target] > targer_quantile]
+    data =  data.loc[data[target] > target_quantile]
     
     assert data == res
