f64bc2eef22f75187eb520578374c3e2e2eadc48
rmr327 <36940292+rmr327@users.noreply.github.com>
2023-09-06 14:41:20 -0400

    Update PandasDescriptiveStatistics.py
    fixed typos

--- src/PandasDescriptiveStatistics.py
+++ src/PandasDescriptiveStatistics.py
@@ -6,13 +6,13 @@ def return_more_than_25th_quantile(df: pd.DataFrame, target: str):
     based on a tatget column (variable) """
 
     target_quantile = df[target].quantile(0.25)
-    df =  df.loc[df[target] > targer_quantile]
+    df =  df.loc[df[target] > target_quantile]
     
     return df
     
 
 if __name__ == '__main__':
     data = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
-    tagert_column = 'sepal_width'
+    target_column = 'sepal_width'
 
-    print(return_df_means(data, target_column))
+    print(return_more_than_25th_quantile(data, target_column))
