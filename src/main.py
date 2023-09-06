7d014d966a8a140790e7cdf370e3c82bc2d98902
rmr327 <36940292+rmr327@users.noreply.github.com>
2023-09-06 14:32:20 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
-"""
-Some code for sanity checking template
-"""
+import pandas as pd
 
-def sanity_add(x, y):
-    return x + y
+
+def return_more_than_25th_quantile(df: pd.DataFrame, target: str):
+    """ Takes in a dataframe and returns only the top 75th perntile of data
+    based on a tatget column (variable) """
+
+    target_quantile = df[target].quantile(0.25)
+    df =  df.loc[df[target] > targer_quantile]
+    
+    return df
+    
 
 if __name__ == '__main__':
-    a = 1
-    b = 2
+    data = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+    tagert_column = 'sepal_width'
 
-    print(sanity_add(a, b))
-  
+    print(return_df_means(data, target_column))
