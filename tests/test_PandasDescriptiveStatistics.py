bb06aa4f54d6b619a121b44584f11b7481d8bb1e
rmr327 <36940292+rmr327@users.noreply.github.com>
2023-09-06 14:38:35 -0400

    Update and rename test_main.py to test_PandasDescriptiveStatistics.py

---
+++ tests/test_PandasDescriptiveStatistics.py
@@ -0,0 +1,12 @@
+from src.PandasDescriptiveStatistics import return_more_than_25th_quantile
+
+def test_return_more_than_25th_quantile():
+    data = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv')
+    tagert_column = 'sepal_width'
+
+    res =  return_more_than_25th_quantile(data, target_column)
+    
+    target_quantile = df[target_column].quantile(0.25)
+    data =  data.loc[data[target] > targer_quantile]
+    
+    assert data == res
--- tests/test_main.py
+++
@@ -1,4 +0,0 @@
-from src.main import sanity_add
-
-def test_sanity_add():
-  assert sanity_add(2, 3) == 5
